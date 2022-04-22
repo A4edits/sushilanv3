599de18d52ac5b11a6ec89a0fed2585a76352661
A4edits <90356972+A4edits@users.noreply.github.com>
2022-04-22 07:49:50 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Abu_ser*:  ```" + msg + "```"
+    return "✅ *aexd-ali*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Abu_ser*:  ```" + msg + "```"
+    return "🛑 *aexd-ali*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Abu_ser*:  ```" + msg + "```"
+    return "⏺️ *aexd-ali*:  ```" + msg + "```"
 }
 
 
